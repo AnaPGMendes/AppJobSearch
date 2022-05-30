d7cb42d3f5037165378ae6d8d61e54a8fd5e32fa
AnaPGMendes <anaclaudia@danim.com.br>
2022-05-30 12:04:48 -0300

    Adicionar arquivos de projeto.

---
+++ JobSearch.API/DataBase/JobSearchContext.cs
@@ -0,0 +1,15 @@
+﻿using JobSearch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobSearch.API.DataBase
+{
+    public class JobSearchContext : DbContext
+    {
+        public JobSearchContext(DbContextOptions<JobSearchContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
